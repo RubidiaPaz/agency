a988f27e3a93a1c57e26c54230615dc24ba7ea10
RubidiaPaz <rubigarcia190@gmail.com>
2022-09-22 11:04:30 -0600

    Creacion de travelAgency

--- App.js
+++ App.js
@@ -1,20 +1,143 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, { useState } from "react";
+import { View, StyleSheet, Image, Text, ScrollView, Modal, Button, TouchableHighlight } from "react-native";
+const App = () => {
+  const[modalVisibleplaya, setModalVisibleplaya]= useState(false);
+  const[modalVisibleplatillo, setModalVisibleplatillo]= useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <>
+      <ScrollView>
+        <Modal transparent={true} animationType="slide" visible={modalVisibleplaya} onRequestClose={()=>{
+          alert('Modal se ha cerrado')
+        }}>
+          <View style={styles.vistaModal}>
+            <View style={styles.Modal}>
+              <Text style={styles.subtitulo}>Ir a la playa</Text>
+              <Text>El Salvador cuenta con hermosas playas a nivel de Centroamérica</Text>
+              <Button title="Cerrar" onPress={()=>{setModalVisibleplaya(!modalVisibleplaya)}}></Button>
+            </View>
+          </View>
+        </Modal>
 
+        
+        <Modal transparent={true} animationType="slide" visible={modalVisibleplatillo} onRequestClose={()=>{
+          alert('Modal se ha cerrado')
+        }}>
+          <View style={styles.vistaModal}>
+            <View style={styles.Modal}>
+              <Text style={styles.subtitulo}>Ir por Platillos tipicos</Text>
+              <Text>El Salvador tiene una varidad de Platillos tipicos muy ricos</Text>
+              <Button title="Cerrar" onPress={()=>{setModalVisibleplatillo(!modalVisibleplatillo)}}></Button>
+            </View>
+          </View>
+        </Modal>
+
+        <View style={{ flexDirection: "row" }}>
+          <Image style={styles.banner} source={require("./src/img/bg.jpg")} />
+        </View>
+
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Que hacer en El Salvador</Text>
+          <ScrollView horizontal>
+            <View>
+              <TouchableHighlight onPress={()=>{setModalVisibleplaya(!modalVisibleplaya)}}>
+              <Image style={styles.ciudad} source={require('./src/img/actividad1.jpg')}/>
+              </TouchableHighlight>
+            </View>
+            <View>
+              <Image style={styles.ciudad} source={require('./src/img/actividad2.jpg')}/>
+            </View>
+            <View>
+              <Image style={styles.ciudad} source={require('./src/img/actividad3.jpg')}/>
+            </View>
+            <View>
+              <Image style={styles.ciudad} source={require('./src/img/actividad4.jpg')}/>
+            </View>
+            <View>
+              <Image style={styles.ciudad} source={require('./src/img/actividad5.jpg')}/>
+            </View>
+          </ScrollView>
+        </View>
+
+        <Text style={styles.titulo}>Platillos Salvadoreños</Text>
+          <View>
+            <TouchableHighlight onPress={()=>{setModalVisibleplatillo(!modalVisibleplatillo)}}>
+              <Image style={styles.mejores} source={require('./src/img/mejores1.jpg')}/>
+            </TouchableHighlight>
+          <View>
+                <Image style={styles.mejores} source={require('./src/img/mejores2.jpg')}/>
+          </View>
+          <View>
+                <Image style={styles.mejores} source={require('./src/img/mejores3.jpg')}/>
+          </View>
+        </View>
+
+        <Text style={styles.titulo}>Rutas Turisticas</Text>
+        <View style={styles.listado}>
+          <View style={styles.listaItem}> 
+                <Image style={styles.mejores} source={require('./src/img/ruta1.jpg')}/>
+          </View>
+          <View style={styles.listaItem}>
+                <Image style={styles.mejores} source={require('./src/img/ruta2.jpg')}/>
+          </View>
+          <View style={styles.listaItem}>
+                <Image style={styles.mejores} source={require('./src/img/ruta3.jpg')}/>
+          </View>
+          <View style={styles.listaItem}>
+                <Image style={styles.mejores} source={require('./src/img/ruta4.jpg')}/>
+          </View>
+        </View>
+
+      </ScrollView>
+    </>
+  );
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  banner: {
+    marginTop:20,
+    height: 250,
+    flex: 1
   },
+  titulo: {
+    fontWeight0:'bold',
+    fontSize:24,
+    marginVertical: 10
+  },
+  contenedor: {
+    marginHorizontal:10
+  },
+  ciudad: {
+    width:250,
+    height:300,
+    marginRight:10
+  },
+  mejores: {
+    width:'100%',
+    height:200,
+    marginVertical:5
+  },
+  listaItem: {
+    flexBasis:'49%',
+  },
+  listado: {
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'space-between'
+  },
+  vistaModal:{
+    backgroundColor:'#000000aa',
+    flex:1
+  },
+  Modal:{
+    backgroundColor:'#fff',
+    margin:50,
+    padding:40,
+    borderRadius:10,
+    flex:1
+  },
+  subtitulo:{
+    fontWeight:'bold',
+    fontSize:14,
+    justifyContent:'center'
+  }
 });
+export default App;
